9244b0ce4ba17dfe6665d2c160224a007ef44798
Garrett Reichman <gsreichman@gmail.com>
2020-02-15 10:51:37 -0500

    created server routes

--- server.js
+++ server.js
@@ -1,6 +1,34 @@
 var express = require('express');
 var path = require('path');
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+
+
+
+app.get("/tables", (req, res) => {
+    res.end("tables");
+});
+
+app.get("/reservations", (req, res) => {
+    res.end("reservations");
+});
+
+app.get("/api/tables", (req, res) => {
+    res.end("api tables");
+});
+
+app.get("/api/wait", (req, res) => {
+    res.end("api wait");
+});
+
+app.get("/*", (req, res) => {
+    res.end("default");
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Listening on port:${PORT}`);
+});
